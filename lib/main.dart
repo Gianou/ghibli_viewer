a7d9a7d761b4d408de5c6238ecb52924f66bab3d
Gianou <david.gianadda@hevs.ch>
2026-08-18 15:34:53 +0200

    Part 2, models and complete film title widget

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:ghibli_viewer/views/movie_title.dart';
+import 'package:ghibli_viewer/models.dart/film_model.dart';
+import 'package:ghibli_viewer/views/film_title.dart';
 
 void main() {
   runApp(const MainApp());
@@ -8,6 +9,32 @@ void main() {
 class MainApp extends StatelessWidget {
   const MainApp({super.key});
 
+  static const mockMovie = Film(
+    id: 'ea660b10-85c4-4ae3-8a5f-41cea3648e3e',
+    title: "Kiki's Delivery Service",
+    originalTitle: '魔女の宅急便',
+    originalTitleRomanised: 'Majo no takkyūbin',
+    image: 'https://image.tmdb.org/t/p/w600_and_h900_bestv2/7nO5DUMnGUuXrA4r2h6ESOKQRrx.jpg',
+    movieBanner:
+        'https://image.tmdb.org/t/p/original/h5pAEVma835u8xoE60kmLVopLct.jpg',
+    description: 'A young witch, on her mandatory year of independent life, finds fitting into a new community difficult while she supports herself by running an air courier service.',
+    director: 'Hayao Miyazaki',
+    producer: 'Hayao Miyazaki',
+    releaseDate: '1989',
+    runningTime: '102',
+    rtScore: '96',
+    people: [
+      'https://ghibliapi.vercel.app/people/2409052a-9029-4e8d-bfaf-70fd82c8e48d',
+      'https://ghibliapi.vercel.app/people/7151abc6-1a9e-4e6a-9711-ddb50ea572ec',
+    ],
+    species: [
+      'https://ghibliapi.vercel.app/species/af3910a6-429f-4c74-9ad5-dfe1c4aa04f2',
+    ],
+    locations: ['https://ghibliapi.vercel.app/locations/'],
+    vehicles: ['https://ghibliapi.vercel.app/vehicles/'],
+    url: 'https://ghibliapi.vercel.app/films/ea660b10-85c4-4ae3-8a5f-41cea3648e3e',
+  );
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -15,10 +42,7 @@ class MainApp extends StatelessWidget {
         body: Center(
           child: Row(
             mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              MovieTitle(title: "Kiki's Delivery Service"),
-              MovieTitle(title: "Howl's Moving Castle"),
-            ],
+            children: [FilmTitle(film: mockMovie)],
           ),
         ),
       ),
---
+++ lib/views/film_title.dart
@@ -0,0 +1,45 @@
+import 'package:flutter/material.dart';
+import 'package:ghibli_viewer/models.dart/film_model.dart';
+
+class FilmTitle extends StatelessWidget {
+  final Film film;
+
+  const FilmTitle({super.key, required this.film});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: 200,
+      padding: const EdgeInsets.all(12),
+      decoration: BoxDecoration(
+        border: Border.all(color: Colors.red, width: 2),
+        borderRadius: BorderRadius.circular(12),
+      ),
+      child: Column(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          ClipRRect(
+            borderRadius: BorderRadius.circular(8),
+            child: Image.network(
+              film.image,
+              width: 170,
+              height: 250,
+              fit: BoxFit.cover,
+            ),
+          ),
+
+          const SizedBox(height: 12),
+
+          Text(
+            film.title,
+            textAlign: TextAlign.center,
+            style: const TextStyle(
+              fontWeight: FontWeight.bold,
+              color: Color.fromARGB(255, 234, 24, 24),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
