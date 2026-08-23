8a6d26562dcd0b79c2986ae5a79489f042fb70aa
Gianou <david.gianadda@hevs.ch>
2026-08-23 13:48:48 +0200

    fix, web build in docs folder, missing base href

--- docs/flutter_bootstrap.js
+++ docs/flutter_bootstrap.js
@@ -42,6 +42,6 @@ _flutter.buildConfig = {"engineRevision":"5f77625673248ee5846fbcaf5d3e1a3878386f
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2807369936" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "2592523548" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
